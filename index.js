2ba929316de85c5ecfcd67f60fc90596d1d28fa6
Monalisa Onyango <+>
2024-06-30 14:40:35 +0300

    scope

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+var customerName='bob';
+console.log(customerName);
+ 
+
+function upperCaseCustomerName() {
+
+    customerName=customerName.toUpperCase(); 
+};
+
+
+function setBestCustomer(){
+ 
+  bestCustomer="not bob"
+}
+
+
+function overwriteBestCustomer(){
+    bestCustomer="maybe bob"
+}
+
+
+const LeastFavoriteCustomer = ''
+
+function changeLeastFavoriteCustomer(){
+    LeastFavoriteCustomer="CAPTAIN"
+}
+
